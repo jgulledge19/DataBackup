03992df41b5650939d54ccfe59bf3015f102c9f9
Josh Gulledge <jgulledge19@hotmail.com>
2014-05-08 11:10:57 -0400

    Updated docs to 1.1.9pl

--- _build/build.transport.databackup.php
+++ _build/build.transport.databackup.php
@@ -51,7 +51,7 @@
 /* Set package info be sure to set all of these */
 define('PKG_NAME','Databackup');
 define('PKG_NAME_LOWER','databackup');
-define('PKG_VERSION','1.1.8');
+define('PKG_VERSION','1.1.9');
 define('PKG_RELEASE','pl');
 define('PKG_CATEGORY','Databackup');
 
